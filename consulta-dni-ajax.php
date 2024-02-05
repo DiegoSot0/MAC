51d6a43a0cd0fdf8534105c50f24d3b92100b6f3
Diego Soto Minaya <66980062+DiegoSot0@users.noreply.github.com>
2024-02-04 21:38:03 -0500

    Update consulta-dni-ajax.php
    arreglo

--- consulta-dni-ajax.php
+++ consulta-dni-ajax.php
@@ -1,84 +1,17 @@
 <?php
 
+$dni = isset($_POST["dni"]) ? htmlspecialchars($_POST["dni"]) : '';
 
-
-
-
-
-$dni=$_POST["dni"];
-
-
-if(strlen($dni)<8 || strlen($dni)>8)
-{
-    $prueba=1;
+if (strlen($dni) != 8) {
+    $prueba = 1;
+} else {
+    // Using try-catch for error handling when making the API request
+    try {
+        $prueba = file_get_contents('https://api.apis.net.pe/v1/dni?numero=' . $dni);
+    } catch (Exception $e) {
+        // Handle the exception (e.g., log the error, set an appropriate value for $prueba)
+        $prueba = 'Error fetching data from the API';
+    }
 }
-else{
-    
-    
-
-   
-        $prueba=file_get_contents('https://api.apis.net.pe/v1/dni?numero='.$dni.'');
-   
-  
-}
-
-
-
-
-
-
-
 
 echo $prueba;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-$dni=$_POST["dni"];
-
-
-if(strlen($dni)<8 || strlen($dni)>8)
-{
-    $prueba=1;
-}
-else{
-    
-    
-
-   
-        $prueba=file_get_contents('https://api.apis.net.pe/v1/dni?numero='.$dni.'');
-   
-  
-}
-
-
-
-
-
-
-
-
-echo $prueba;
-
-
-
-
-
-*/
