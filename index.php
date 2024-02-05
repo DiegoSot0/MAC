21b8cbeaf382c2f82eb753e2293e78f959cfb59a
Diego Soto Minaya <66980062+DiegoSot0@users.noreply.github.com>
2024-02-05 00:02:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,6 @@
             font-family: FontAwesome;
             font-style: normal;
             color: #FFF;
-
         }
     </style>
 
@@ -54,9 +53,11 @@
                     <img src="maclogo.png" alt="Imagen" class="img-fluid m-5">
                 </div>
                 <div class="input-group mb-3">
-                    <input type="text" id="dni" autocomplete="off" name="dni" class="form-control text-large" oninput="validarDNI(this)" inputmode="numeric" required>
+                    <input type="text" id="dni" autocomplete="off" name="dni" class="form-control text-large"
+                        oninput="validarDNI(this)" inputmode="numeric" required>
                     <div class="input-group-append">
-                        <button id="clearDNI" class="btn btn-danger btn-outline-secondary" type="button"><i class="fas fa-times"></i></button>
+                        <button id="clearDNI" class="btn btn-danger btn-outline-secondary" type="button"><i
+                                class="fas fa-times"></i></button>
                     </div>
                 </div>
                 <button id="prueba" class="btn btn-primary m-2 p-2 text-large">Consultar</button>
@@ -70,13 +71,11 @@
                 <label id="errorMensaje" class="text-large" style="color: red;"></label>
             </div>
             <div>
-                <button id="copiarNombre" class="btn btn-warning m-4 p-2 text-large" style="display: none;">Copiar
-                    Nombre
-                </button>
-                <button id="copiarDNI" class="btn btn-info m-4 p-2 text-large" style="display: none;">Copiar
-                    DNI</button>
+                <button id="copiarNombre" class="btn btn-warning m-4 p-2 text-large"
+                    style="display: none;">Copiar Nombre</button>
+                <button id="copiarDNI" class="btn btn-info m-4 p-2 text-large"
+                    style="display: none;">Copiar DNI</button>
             </div>
-
         </div>
     </div>
 
@@ -88,37 +87,37 @@
 
             $.ajax({
                 type: "POST",
-                url: "consulta-dni-ajax.php",
+                url: "/consulta-dni-ajax.php",
                 data: {
                     dni: dni,
-                    _token: $('meta[name="csrf-token"]').attr('content') // Obtener el token CSRF de la página
+                    _token: $('meta[name="csrf-token"]').attr('content')
                 },
-                dataType: 'json',
-                success: function(data) {
-                    if (data.error) {
-                        $("#nombreCompleto").empty();
-                        $("#errorMensaje").html('El DNI tiene que tener 8 dígitos');
-                        $("#copiarNombre").hide();
-                        $("#copiarDNI").hide();
-                        if (!segundaBusquedaRealizada) {
-                            realizarSegundaBusqueda(dni);
-                        }
-                    } else {
-                        $("#errorMensaje").empty();
-                        console.log(data);
-                        $("#nombreCompleto").html(data.nombres + " " + data.apellidoPaterno + " " + data
-                            .apellidoMaterno);
-                        $("#copiarNombre").show();
-                        $("#copiarDNI").show();
-                    }
-                },
-                error: function() {
+                dataType: 'json'
+            })
+            .done(function(data) {
+                if (data.error) {
                     $("#nombreCompleto").empty();
+                    $("#errorMensaje").html('El DNI tiene que tener 8 dígitos');
                     $("#copiarNombre").hide();
                     $("#copiarDNI").hide();
                     if (!segundaBusquedaRealizada) {
                         realizarSegundaBusqueda(dni);
                     }
+                } else {
+                    $("#errorMensaje").empty();
+                    console.log(data);
+                    $("#nombreCompleto").html(data.nombres + " " + data.apellidoPaterno + " " + data
+                        .apellidoMaterno);
+                    $("#copiarNombre").show();
+                    $("#copiarDNI").show();
+                }
+            })
+            .fail(function() {
+                $("#nombreCompleto").empty();
+                $("#copiarNombre").hide();
+                $("#copiarDNI").hide();
+                if (!segundaBusquedaRealizada) {
+                    realizarSegundaBusqueda(dni);
                 }
             });
         }
@@ -127,7 +126,7 @@
             consultarDNI();
         });
 
-        $("#dni").keypress(function(event) {
+        $("#dni").keydown(function(event) {
             if (event.which === 13) { // Si se presiona la tecla "Enter"
                 consultarDNI();
                 event.preventDefault(); // Evita que el formulario se envíe (si es un formulario)
@@ -148,7 +147,6 @@
 
             // Elimina el elemento temporal
             document.body.removeChild(input);
-
         });
         $("#copiarDNI").click(function() {
             var dni = $("#dni").val();
@@ -185,35 +183,34 @@
         }
 
         function realizarSegundaBusqueda(dni) {
-            var csrfToken = $("#csrf_token").val(); // Assuming you have a CSRF token input field
 
             $.ajax({
-            type: "POST",
-            url: "segundo-json",
-            data: {
-                dni: dni,
-                _token: $('meta[name="csrf-token"]').attr('content')
-            },
-            dataType: 'json',
-                success: function(data) {
-                    if (!data.error) {
-                        if (data.NOMBRE.trim() == "") {
-                            $("#errorMensaje").html('DNI NO REGISTRADO');
-                        } else {
-                            // Data found in the second JSON
-                            $("#errorMensaje").empty();
-                            console.log(data);
-                            $("#nombreCompleto").html(data.NOMBRE + " " + data.APELLIDO_PATERNO + " " + data.APELLIDO_MATERNO);
-                            $("#copiarNombre").show();
-                            $("#copiarDNI").show();
-                        }
-                    } else {
-                        $("#errorMensaje").html('DNI NO REGISTRADO');
-                    }
+                type: "POST",
+                url: "/segundo-json.php",
+                data: {
+                    dni: dni,
+                    _token: $('meta[name="csrf-token"]').attr('content')
                 },
-                error: function() {
-                    $("#errorMensaje").html('Error en la búsqueda del segundo JSON');
+                dataType: 'json'
+            })
+            .done(function(data) {
+                if (!data.error) {
+                    if (data.NOMBRE.trim() == "") {
+                        $("#errorMensaje").html('DNI NO REGISTRADO');
+                    } else {
+                        // Data found in the second JSON
+                        $("#errorMensaje").empty();
+                        console.log(data);
+                        $("#nombreCompleto").html(data.NOMBRE + " " + data.APELLIDO_PATERNO + " " + data.APELLIDO_MATERNO);
+                        $("#copiarNombre").show();
+                        $("#copiarDNI").show();
+                    }
+                } else {
+                    $("#errorMensaje").html('DNI NO REGISTRADO');
                 }
+            })
+            .fail(function() {
+                $("#errorMensaje").html('Error en la búsqueda del segundo JSON');
             });
         }
     </script>
